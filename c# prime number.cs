83b1d6716eb7763e462401afae066cbefd0edcb6
sherbi15 <113832425+sherbi15@users.noreply.github.com>
2022-11-29 23:16:10 +0530

    Create c# prime number.cs

---
+++ c# prime number.cs
@@ -0,0 +1,21 @@
+using System;  
+  public class Program  
+   {  
+     public static void Main(string[] args)  
+      {  
+          int n, i, m=0, flag=0;    
+          n = int.Parse(Console.ReadLine());  
+          m=n/2;    
+          for(i = 2; i <= m; i++)    
+          {    
+           if(n % i == 0)    
+            {    
+             Console.Write("not Prime");    
+             flag=1;    
+             break;    
+            }    
+          }    
+          if (flag==0)    
+           Console.Write("Prime");       
+     }  
+   }  
